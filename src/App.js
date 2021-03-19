11b5de493870cdb434500192ae574a2f4ca7439c
Alex <sudo dnf install git-all>
2021-03-19 10:48:01 +0300

    created the right structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Appointment from "./Components/appointments/Appointment";
-import SignIn from "./Components/signIn/SignIn";
-import Registration from "./Components/regestration/Registration";
+import Appointment from "./Components/Appointments/Appointment";
+import SignIn from "./Components/SignIn/SignIn";
+import Registration from "./Components/Registration/Registration";
 import "./App.scss";
 
 function App() {
@@ -11,17 +11,17 @@ function App() {
       <Switch>
         <Route path="/appointment" component={Appointment} />
         <Route path="/registration" component={Registration} />
-        <Route path="/" component={SignIn} />
+        <Route path="/autorization" component={SignIn} />
       </Switch>
 
       {localStorage.getItem("user") ? (
         <Switch>
           <Redirect to="/appointment" />
         </Switch>
-      ) : (
-        <Switch>
-          <Redirect to="/" />
-        </Switch>
+      ) : window.location.href ===
+        "http://localhost:3000/autorization" ? null : window.location.href ===
+        "http://localhost:3000/registration" ? null : (
+        <Redirect from="/" to="/autorization" />
       )}
     </div>
   );
